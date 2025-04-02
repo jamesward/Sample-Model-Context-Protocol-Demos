c046d8e44123c8901e8f631d969d1fe72b4946a6
James Ward <james@jamesward.com>
2025-04-02 14:19:51 -0600

    move spring-ai-agent-ecs from gradle to maven

--- modules/spring-ai-agent-ecs/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    kotlin("jvm") version "2.1.20" apply false
-    kotlin("plugin.spring") version "2.1.20" apply false
-    id("org.springframework.boot") version "3.4.4" apply false
-    id("io.spring.dependency-management") version "1.1.7" apply false
-}
