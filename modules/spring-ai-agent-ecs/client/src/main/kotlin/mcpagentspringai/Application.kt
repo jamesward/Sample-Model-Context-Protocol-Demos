7f9c713b8cb111598ab9081545ccd5fc58068826
James Ward <james@jamesward.com>
2025-04-10 17:43:27 -0600

    bump to Spring AI 1.0.0-M7

--- modules/spring-ai-agent-ecs/client/src/main/kotlin/mcpagentspringai/Application.kt
+++ modules/spring-ai-agent-ecs/client/src/main/kotlin/mcpagentspringai/Application.kt
@@ -1,10 +1,8 @@
 package mcpagentspringai
 
-import io.modelcontextprotocol.client.McpClient
-import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport
+import io.modelcontextprotocol.client.McpSyncClient
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -15,14 +13,9 @@ import org.springframework.web.bind.annotation.RestController
 @SpringBootApplication
 class Application {
     @Bean
-    fun chatClient(@Value("\${mcp-service.url:http://localhost:8081}") url: String, builder: ChatClient.Builder): ChatClient {
-        val mcpClient = McpClient
-            .sync(HttpClientSseClientTransport(url))
-            .build()
-        mcpClient.initialize()
-
+    fun chatClient(mcpSyncClients: List<McpSyncClient>, builder: ChatClient.Builder): ChatClient {
         return builder
-            .defaultTools(SyncMcpToolCallbackProvider(mcpClient))
+            .defaultTools(SyncMcpToolCallbackProvider(mcpSyncClients))
             .build()
     }
 }
